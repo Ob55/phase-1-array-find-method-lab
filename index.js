f063c2c10c35bb2ae3a599f70c2cd444ff37c0e7
Ob55 <brian55mwangi@gmail.com>
2023-12-18 21:54:00 +0300

    Done assigment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+ function superbowlWin(record) {
+  const winning = record.find(entry => entry.result === 'W');
+  return winning ? winning.year : null;
+}
+function superbowlWin(record) {
+     const winning = record.find(entry => entry.result === 'W');
+     return winning ? winning.year : undefined;
+   }
