d98520886b6f4346995587d5001354d8468193c3
Ob55 <brian55mwangi@gmail.com>
2023-12-18 22:00:25 +0300

    Done assigment

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
   return winning ? winning.year : null;
 }
 function superbowlWin(record) {
-     const Record = record.find(entry => entry.result === 'W');
-     return Record ? Record.year : undefined;
+     const winning = record.find(entry => entry.result === 'W');
+     return  winning ? winning.year : undefined;
    }
